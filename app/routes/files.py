c88b09403e68778be2ec4c9c4d2dc2dd2fd14737
Alex Constantinou <a_constantinou@hotmail.co.uk>
2021-12-09 20:41:03 +0000

    route for editing files added

--- app/routes/files.py
+++ app/routes/files.py
@@ -35,6 +35,31 @@ def save_file():
         filename=file.filename,
     )
 
+@files.route('/files', methods=['PUT'])
+def edit_file():
+    print('request', request.form)
+    new_file = request.files.get('NewFile', None)
+    file_to_delete = request.form.get('FileToDelete', None)
+    filename = None
+    if file_to_delete:
+        print(file_to_delete)
+        damnation_bucket.delete_objects(
+            Delete={
+                'Objects': [
+                    {
+                        'Key': file_to_delete,
+                    },
+                ],
+            },
+        )
+    if new_file:
+        damnation_bucket.Object(new_file.filename).put(Body=new_file.read())
+        filename = new_file.filename
+    return jsonify(
+        status=True,
+        filename=filename,
+    )
+
 
 @files.route('/files/<filename>')
 def get_file(filename):
